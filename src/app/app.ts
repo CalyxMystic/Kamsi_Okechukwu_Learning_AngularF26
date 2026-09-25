8a054266e49d1cac725292384336e2e1fbbe8f93
CalyxMystic <kaykamsy02@gmail.com>
2026-09-25 00:34:39 -0400

    I changed the version of package.json and I started the server I added a new interface and added a union and optional variables Added my scss and did the html Changed my package.json to 0.2.0 and made a new branch

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {A2} from './shared/models/a2';
+import {Game} from './shared/models/game';
 @Component({
   imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.scss',
+  styleUrl: './game-card.scss',
   templateUrl: './app.html',
 })
 export class App {
-  appList: A2[] =[
-    {id:1, name:"Kamsi", hasLocation:true, union:"yes"},
-    {id:2, name:"Okechukwu", hasLocation:false, union:0},
-    {id:3, name:"Kycee", hasLocation:true, union:"yes"},
-    {id:4, name:"Loki", hasLocation:false, union:69},
-    {id:5, name:"Fenrir", hasLocation:true, union:"no", hasFriends:false},
-    {id:6, name:"Bjorn", hasLocation:false, union:78, hasFriends:true},
-    ]
+  gameList: Game[] = [
+    {id: 1, name: "Uncharted 4: A thief's End", genre: "Action-adventure", copiesSold: " Over 18 million", isOwned: true},
+    {id: 2, name: "God of War Ragnarok", genre: "Action-adventure", copiesSold: "Over 15 million", isOwned: false},
+    {id: 3, name: "Nine Sols", genre: "2D Action-platformer / Metroidvania", copiesSold: "Over 800,000", isOwned: false},
+    {id: 4, name: "Black Myth Wukong", genre: "Action RPG", copiesSold: "Over 30 million", isOwned: false},
+    {id: 5, name: "Cyberpunk 2077:Phantom Liberty", genre: "Action RPG", copiesSold: "Over 15 million", isOwned: true},
+    {id: 6, name: "Prince of Persia: The Lost Crown", genre: "Action-adventure / Metroidvania", copiesSold: "Over 1.3 million", isOwned: true},
+  ]
 }
--- src/app/shared/models/a2.ts
+++
@@ -1,7 +0,0 @@
-export interface A2 {
-  id:number
-  name:string
-  hasLocation:boolean
-  union:string | number
-  hasFriends?:boolean
-}
