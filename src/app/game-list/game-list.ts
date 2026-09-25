373e9a289bea1c4e7482649f94c8b99d900d917d
CalyxMystic <kaykamsy02@gmail.com>
2026-09-25 02:39:23 -0400

    I changed the version of package.json and I started the server I added a new interface and added a union and optional variables Added my scss and did the html Changed my package.json to 0.2.0 and made a new branch Made two new components Added gameList and gameList Items with input Added a switch case and if statements with css to my game-list

--- src/app/game-list/game-list.ts
+++ src/app/game-list/game-list.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import {GameListItem} from '../game-list-item/game-list-item';
+import {Game} from '../shared/models/game';
 
 @Component({
-  imports: [],
+  imports: [
+    GameListItem
+  ],
   selector: 'app-game-list',
   styleUrl: './game-list.css',
   templateUrl: './game-list.html',
 })
 export class GameList {
-  gameList=[
+  gameCard:Game[]=[
     {id:1, name: "Clair Obscur:Expedition 33", genre: "Role-playing", copiesSold: "Over 8 million", isOwned: false},
     {id:2, name:"Sword Of The Sea", genre: "Action-Adventure",copiesSold:"Over 36,00",isOwned:false},
     {id:3, name:"Persona 3", genre:"Action-Adventure",copiesSold:"Over 3 million",isOwned:false},
