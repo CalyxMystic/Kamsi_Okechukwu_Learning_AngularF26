2d0f3f7ffa04e893947805bba73170459ff8301b
CalyxMystic <kaykamsy02@gmail.com>
2026-09-25 00:46:32 -0400

    I changed the version of package.json and I started the server I added a new interface and added a union and optional variables Added my scss and did the html Changed my package.json to 0.2.0 and made a new branch Made two new components

---
+++ src/app/game-list-item/game-list-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-game-list-item',
+  styleUrl: './game-list-item.css',
+  templateUrl: './game-list-item.html',
+})
+export class GameListItem {}
---
+++ src/app/game-list/game-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-game-list',
+  styleUrl: './game-list.css',
+  templateUrl: './game-list.html',
+})
+export class GameList {}
